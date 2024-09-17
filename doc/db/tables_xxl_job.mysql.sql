b9abfa7cbfef0eb0a2cb390f0fd209147f0cdc10
Bulain Peng <bulain@gmail.com>
2024-09-17 23:28:02 +0800

    多数据库的支持：mssql,oracle,pgsql

--- doc/db/tables_xxl_job.mysql.sql
+++ doc/db/tables_xxl_job.mysql.sql
@@ -1,9 +1,6 @@
-#
-# XXL-JOB v2.4.1
-# Copyright (c) 2015-present, xuxueli.
-
-CREATE database if NOT EXISTS `xxl_job` default character set utf8mb4 collate utf8mb4_unicode_ci;
-use `xxl_job`;
+---#
+---# XXL-JOB v2.4.1
+---# Copyright (c) 2015-present, xuxueli.
 
 SET NAMES utf8mb4;
 
