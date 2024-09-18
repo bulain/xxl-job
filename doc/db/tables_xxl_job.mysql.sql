f1af254df22ea73fddb88ed427ecd01dc4e2529f
Bulain Peng <bulain@gmail.com>
2024-09-18 12:25:16 +0800

    增强密码安全性，调整密码长度

--- doc/db/tables_xxl_job.mysql.sql
+++ doc/db/tables_xxl_job.mysql.sql
@@ -98,7 +98,7 @@ CREATE TABLE `xxl_job_group` (
 CREATE TABLE `xxl_job_user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL COMMENT '账号',
-  `password` varchar(50) NOT NULL COMMENT '密码',
+  `password` varchar(100) NOT NULL COMMENT '密码',
   `role` tinyint(4) NOT NULL COMMENT '角色：0-普通用户、1-管理员',
   `permission` varchar(255) DEFAULT NULL COMMENT '权限：执行器ID列表，多个逗号分割',
   PRIMARY KEY (`id`),
